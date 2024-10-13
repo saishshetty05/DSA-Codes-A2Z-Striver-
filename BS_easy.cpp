69a1d2b2275d1492edcafbe741eb5788670f2918
saishshetty05 <146539990+saishshetty05@users.noreply.github.com>
2024-10-13 20:05:36 +0530

    13 oct

--- BS_easy.cpp
+++ BS_easy.cpp
@@ -80,3 +80,28 @@ public:
     }
 };
 
+// ------------------------------ Search Insert Position---------------
+
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n =nums.size();
+        int low = 0, high = n - 1;
+      
+        
+        while (low <= high) {
+            int mid = (low+high)/2;
+
+            if(nums[mid]==target){return mid;}
+            
+            else if (nums[mid] < target) {
+                                low = mid + 1; 
+            } else {
+                high = mid - 1;  
+            }
+        }
+        
+        return low;
+    }
+};
