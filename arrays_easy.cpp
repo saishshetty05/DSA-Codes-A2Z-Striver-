aa8c4cb92b6ede4e90131f21a796825a3d9accda
Saish Shetty <146539990+saishshetty05@users.noreply.github.com>
2024-10-17 22:47:54 +0530

    17th oct Dutch flag algorithm

--- arrays_easy.cpp
+++ arrays_easy.cpp
@@ -177,3 +177,51 @@ public:
       
     }
 };
+
+// --------------------Dutch Flag algorithm--------------------
+//low: represents the rightmost boundary of 0s
+// mid: current element being examined
+// high: represents the leftmost boundary of 2s
+// Three-Way Partitioning:
+
+// All elements before low are 0s
+// All elements after high are 2s
+// All elements between low and mid are 1s
+
+
+// How it Works:
+
+// When we find 0: swap with low pointer element and increment both low and mid
+// When we find 1: just increment mid
+// When we find 2: swap with high pointer element and decrement high
+
+
+// Time Complexity: O(n) where n is the size of array
+// Space Complexity: O(1) as we're doing it in-place
+
+//
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low =0 , mid=0,high= nums.size() - 1;
+        while(mid<=high)
+        {
+            if(nums[mid]==0){
+                        swap(nums[low],nums[mid]);
+                        low++;
+                        mid++;
+                }
+            else if(nums[mid]==1)
+            {
+                mid++;
+            }
+            else
+            {
+                swap(nums[mid],nums[high]);
+                high--;
+            }
+        }
+
+    }
+};
