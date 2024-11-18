2e743dc923877fec0512db7d9be1c18c1869a1a4
Saish Shetty <146539990+saishshetty05@users.noreply.github.com>
2024-11-18 10:23:30 +0530

    dll deletion

--- doubly_LL/easy.cpp
+++ doubly_LL/easy.cpp
@@ -15,3 +15,72 @@ class Solution {
         
     }
 };
+
+
+// --------------- delete a doublyLL node ----------------
+//---------------1)at head---2)at tail----3) at kth position-------
+class Solution {
+  public:
+       Node* deleteNode(Node* head, int x) {
+        // Your code here
+        Node* temp = head;
+        
+        int count = 0;
+        while(temp != nullptr)
+        {
+            count += 1;
+            if(count == x) break;
+            temp = temp->next;
+        }
+        Node* prev = temp->prev;
+        Node* front = temp->next;
+        
+        if(front == nullptr && prev == nullptr)  
+        {
+            return nullptr;  
+        }
+        else if(prev == nullptr) 
+        {
+            return deleteHead(head);
+        }
+        else if(front == nullptr)  
+        {
+            return deleteTail(head);
+        }
+        else
+        {
+            prev->next = front;
+            front->prev = prev;
+            temp->next = nullptr;
+            temp->prev = nullptr;
+            free(temp);
+            return head;   
+        }
+    }
+    
+    Node* deleteHead(Node* head){
+        Node* temp = head;
+        head = head->next;
+        if(head != nullptr)     
+            head->prev = nullptr;
+        temp->prev = nullptr;
+        temp->next = nullptr;
+        delete temp;
+        return head;
+    }
+    
+    Node* deleteTail(Node* head)
+    {
+        Node* temp = head;
+        while(temp->next != nullptr)
+        {
+            temp = temp->next;
+        }
+        Node* prev = temp->prev;
+        prev->next = nullptr;
+        temp->prev = nullptr;
+        temp->next = nullptr;   
+        delete temp;
+        return head;
+    }
+};
